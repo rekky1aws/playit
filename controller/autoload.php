6a30074af5cdacf81aefe525bda81e25e24b52f8
rekky1aws <killikomfr@gmail.com>
2023-05-02 21:18:25 +0200

    Updated Autoloader
    Changed autoloader to be more efficient and not cause errors.
    It will now check if a filename is a folder before checking what's
    inside

--- controller/autoload.php
+++ controller/autoload.php
@@ -2,33 +2,29 @@
 
 function loadFiles ($path)
 {
-	// Scanning files in given pass, returns false if path is not a folder
-	$files = scandir($path);
+	// Checking if path is a folder
+	if (is_dir($path)) {
+		// Scanning files in given pass, returns false if path is not a folder
+		$files = scandir($path);
 
-	// If path isn't a folder
-	if (!$files) {
-		return false;
-	}
-
-	foreach ($files as $file) {
-
-		// Excluding folders starting with a .
-		if ($file[0] !== '.') {
-			
-			// If php file, include it
-			if (str_contains($file, ".php")) {
-				include_once $path . "/" . $file;
-			}
-
-			// If something else, recursively include php content
-			else {
+		foreach ($files as $file) {
+			// Excluding folders starting with a .
+			if ($file[0] !== '.') {
 				loadFiles($path . "/" . $file);
 			}
+		}
+
+	} else {
+		// If php file, include it
+		if (str_contains($path, ".php")) {
+			include_once $path;
 		}
 	}
 }
 
+// echo "<pre>";
 loadFiles(".");
+// echo "</pre>";
 
 
 ?>
