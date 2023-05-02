8b12b049040fe89fb2496b182510a1fd7402f909
rekky1aws <killikomfr@gmail.com>
2023-05-02 21:16:31 +0200

    Created script folder
    Created script/ folder to put Javascript code and app.js
    Added main CSS and JS inclusions in head.php

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ namespace App;
 
 include_once("./controller/autoload.php");
 
+// use App\Head;
+
+// $head = new Head();
 
 ?>
 
@@ -13,6 +16,7 @@ include_once("./controller/autoload.php");
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="/style/main.css">
+	<script type="text/javascript" src="/script/app.js" defer></script>
 	<title> Playit </title>
 </head>
 <body>
